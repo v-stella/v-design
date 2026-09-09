95c7c474a318f63556de9bae3e1af482386ff185
vv <vvwvv789@gmail.com>
2026-09-09 16:36:45 +0800

    fix(docs): remove inherited Carbon Ads integration

--- apps/v4/pages/docs/[...slug].vue
+++ apps/v4/pages/docs/[...slug].vue
@@ -127,9 +127,6 @@ useSeoMeta({
           <DocsTableOfContents :toc="page.body.toc" />
           <div class="h-12" />
         </div>
-        <div class="flex flex-1 flex-col gap-12 px-6">
-          <CarbonAds />
-        </div>
       </div>
     </div>
   </template>
